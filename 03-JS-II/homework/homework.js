a02bd8491315d99432bc191b2406ebd1f838ca54
Sergio Poto <sergiosebastianpoto@gmail.com>
2022-06-27 10:03:10 -0300

    homework 03-JS-II completada

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -162,7 +162,7 @@ function esPrimo(numero) {
   if (numero < 2) {
     return false;
   }
-  for (var i = 2; i <= numero; i++) {
+  for (var i = 2; i < numero; i++) {
     if (numero % i === 0) {
       return false;
     }
